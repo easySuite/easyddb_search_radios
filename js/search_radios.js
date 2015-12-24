d08f519671b75e10fa4f77829ca243d79500f07c
Nicolae Grigoriu <ng@inlead.dk>
2015-12-24 18:40:04 +0200

    DDBEASY-730 - Hide radios on search pages.

--- js/search_radios.js
+++ js/search_radios.js
@@ -5,6 +5,13 @@
 
 (function ($) {
   $(document).ready(function() {
+    var path = window.location.pathname;
+    var arg = path.split('/')[2];
+    // Unset advanced form.
+    if (arg === 'search') {
+      $('#search-block-form #edit-advanced').remove();
+    }
+
     $('#search-block-form').submit(function(event) {
       var $advanced_search = $('fieldset#edit-advanced');
       var $search_radios = $('div[id^="search-radios"]').find('input');
