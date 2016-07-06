53de5710f10ea90a2f45ecbd733d174ab9c64716
Nicolae Grigoriu <ng@inlead.dk>
2016-07-06 16:18:07 +0300

    EASYOPAC-208 - Use generic search radios search.

--- js/search_radios.js
+++ js/search_radios.js
@@ -1,38 +1,31 @@
 /**
  * @file
- *  Frontend functionality for search radios.
+ * Frontend functionality for search radios.
  */
 
 (function ($) {
-  $(document).ready(function() {
-    $('#edit-searches').find('a').on('click', function() {
-      $(this).parents('.form-item-searches').find('input[name="searches"]').prop("checked", true);
-      return false;
-    });
-    var path = window.location.pathname;
-    var arg = path.split('/')[2];
-    // Unset advanced form.
-    if (arg === 'search') {
-      $('#search-block-form #edit-advanced').remove();
+  'use strict';
+
+  Drupal.behaviors.easyddb_search_radios = {
+    attach: function () {
+      $('#edit-searches').find('a').on('click', function() {
+        $(this).parents('.form-item-searches').find('input[name="searches"]').prop("checked", true);
+
+        return false;
+      });
+
+      // Redirect to the search page.
+      $('#search-block-form').submit(function(event) {
+        var search_radio = $('div[id^="search-radios"]').find('input').filter(":checked").val();
+
+        if (search_radio != 'ting') {
+          event.preventDefault();
+
+          var query_string = $('input[id^="edit-search-block-form"][type="text"]').val();
+          var current_url = window.location.origin;
+          window.location.assign(current_url + '/search/' + search_radio + '/' + query_string);
+        }
+      });
     }
-
-    $('#search-block-form').submit(function(event) {
-      var $advanced_search = $('fieldset#edit-advanced');
-      var $search_radios = $('div[id^="search-radios"]').find('input');
-      if ($advanced_search.hasClass('collapsed')) {
-        event.preventDefault();
-        var $query_string = $('input[id^="edit-search-block-form"][type="text"]').val();
-        var selected_search;
-        var current_url = window.location.origin;
-
-        $($search_radios).each(function (index, element) {
-          if ($(element).is(":checked")) {
-            selected_search = $(element).val();
-          }
-        });
-
-        window.location.assign(current_url + '/' + selected_search + '/' + $query_string);
-      }
-    });
-  });
+  };
 })(jQuery);
